cc0a8218272594c0ff02f0d1474beeeef33bc0c4
Mabarik667f <jc.bob1412@gmail.com>
2025-01-03 20:21:12 +0300

    del: delete accounts router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from api.v1.endpoints import cr_router, accounts_router, observer_router, auth_router
+from api.v1.endpoints import cr_router, observer_router, auth_router
 
 v1_router = APIRouter()
 
 v1_router.include_router(cr_router)
-v1_router.include_router(accounts_router)
 v1_router.include_router(observer_router)
 v1_router.include_router(auth_router)
--- src/api/v1/endpoints/__init__.py
+++ src/api/v1/endpoints/__init__.py
@@ -1,12 +1,10 @@
 __all__ = (
     "cr_router",
-    "accounts_router",
     "observer_router",
     "auth_router",
 )
 
 
 from .cryptowrapper import router as cr_router
-from .accounts import router as accounts_router
 from .observer import router as observer_router
 from .auth import router as auth_router
